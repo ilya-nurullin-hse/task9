45df79a0c75110726312932928b9ac34e0d98491
Ilya Nurullin <nurullinin@mail.ru>
2017-08-06 20:54:28 +0500

    The first version of solution

---
+++ Task9/List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class List
+    {
+        public List next;
+        public String data;
+
+        public List(string data, List next = null)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public List map(Func<String, String> f)
+        {
+            if (next == null)
+                return new List(f(data), null);
+            return new List(f(data), next.map(f));
+        }
+    }
+}
--- Task9/Program.cs
+++ Task9/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task9
 {
@@ -10,6 +7,68 @@ namespace Task9
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Введите значения информационных полей элементов списка через пробел: ");
+                string input = Console.ReadLine();
+
+                var items = input.Split();
+                List list = StringArrayToItem(items);
+
+                Console.WriteLine("ReverseData: ");
+                PrintList(ReverseData(list));
+
+                Console.WriteLine("ReverseDataRecursion: ");
+                PrintList(ReverseDataRecursion(list));
+
+                Console.WriteLine("Map: ");
+                PrintList(list.map(s => new string(s.Reverse().ToArray())));
+
+                Console.WriteLine();
+            }
+        }
+
+        static List ReverseData(List list)
+        {
+            var currentList = list;
+            var headItem = new List(new string(currentList.data.Reverse().ToArray()));
+            var newItem = headItem;
+
+            while (currentList.next != null)
+            {
+                currentList = currentList.next;
+                newItem.next = new List(new string(currentList.data.Reverse().ToArray()));
+                newItem = newItem.next;
+            }
+
+            return headItem;
+        }
+
+        static List ReverseDataRecursion(List list)
+        {
+            List newList = new List(new string(list.data.Reverse().ToArray()));
+            if (list.next != null)
+                newList.next = ReverseDataRecursion(list.next);
+            return newList;
+        }
+
+        static List StringArrayToItem(string[] items)
+        {
+            List newList = new List(items.First());
+            if (items.Length > 1)
+                newList.next = StringArrayToItem(items.Skip(1).ToArray());
+            return newList;
+        }
+
+        static void PrintList(List list)
+        {
+            if (list == null)
+                Console.WriteLine("null");
+            else
+            {
+                Console.Write($"{list.data}->");
+                PrintList(list.next);
+            }
         }
     }
 }
